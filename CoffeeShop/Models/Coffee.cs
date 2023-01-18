015d7a66951b62c06b1aaa7d2226443c57bb1530
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-01-18 13:23:08 -0600

    initial commit

---
+++ CoffeeShop/Models/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int BeanVarietyId { get; set; }
+        public string BeanVariety { get; set; }
+
+    }
+}
